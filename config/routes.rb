c32e73a9cdd3e8315f9858899ee7dec8515058ec
Brett Puddick <39726917+brett-puddick@users.noreply.github.com>
2022-07-04 19:57:42 +0100

    Basic Views created. Tailwind installed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'home#index'
+  get '/about/', to: 'about#index'
+  get '/contact/', to: 'contact#index'
+  get '/projects', to: 'projects#index'
+  get '/projects/:project_id', to: 'projects#show'
+  get '/blog/', to: 'blog#index'
+  get 'blog/:blog_id', to: 'blog#show'
 end
